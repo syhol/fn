64412840de033648c4cdf1e9a6d96d5ecc47aaef
Simon Holloway <simon@syhol.io>
2015-10-09 14:10:24 +0100

    New facade class

---
+++ FnFacade.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Syhol\Fn;
+
+
+class FnFacade
+{
+    protected static $global;
+
+    protected $factory;
+
+    protected $container;
+
+    public function __construct(FnFactory $factory, FnContainer $container)
+    {
+        $this->factory = $factory;
+        $this->container = $container;
+        $container->set('fnFactory', $factory->parse([$factory, 'parse']));
+        $container->set('fnContainer', $this->poly([$this, 'set'], [$this, 'get']));
+    }
+
+    public function set($key, callable $fn)
+    {
+        $this->container->set($key, $this->factory->parse($fn));
+
+        return $this;
+    }
+
+    public function get($key)
+    {
+        return $this->container->get($key);
+    }
+
+    public function parse(callable $callable)
+    {
+        return $this->factory->parse($callable);
+    }
+    
+    public function poly(array $items)
+    {
+        return $this->factory->poly($items);
+    }
+
+    public static function setGlobal(FnContainer $container)
+    {
+        return self::$global = $container;
+    }
+
+    public static function getGlobal()
+    {
+        return self::$global;
+    }
+}
